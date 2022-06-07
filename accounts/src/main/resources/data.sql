1f8ac6f7e4ea6e60ad3423d1d37c2e8606e0e765
Diego Rodrigues de Sousa <diegors90@gmail.com>
2022-06-06 23:35:04 -0300

    fix: table name

--- accounts/src/main/resources/data.sql
+++ accounts/src/main/resources/data.sql
@@ -5,10 +5,10 @@ CREATE TABLE `customer` (
   `id` int AUTO_INCREMENT  PRIMARY KEY,
   `name` varchar(100) NOT NULL,
   `number` varchar(20) NOT NULL,
-  `createtAt` date DEFAULT NULL
+  `created_at` date DEFAULT NULL
 );
 
-INSERT INTO `customer` (`name`, `number`,`createtAt`)
+INSERT INTO `customer` (`name`, `number`,`created_at`)
  VALUES ('Kakashi','9993823234', CURDATE());
 
 CREATE TABLE `account` (
@@ -16,8 +16,8 @@ CREATE TABLE `account` (
   `customer_id` int,
   `number` varchar(20) NOT NULL,
   `type` varchar(100) NOT NULL,
-  `createtAt` date DEFAULT NULL
+  `created_at` date DEFAULT NULL
 );
 
-INSERT INTO `account` (`customer_id`, `number`, `type`, `createtAt`)
+INSERT INTO `account` (`customer_id`, `number`, `type`, `created_at`)
  VALUES (1, 186576453, 'SAVING', CURDATE());
